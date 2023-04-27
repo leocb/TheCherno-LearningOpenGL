14f78136ac0f17445aa801ab7f5d14e8c51db2f0
Leonardo Bottaro <leonardo.bottaro@oracle.com>
2023-04-27 01:50:47 -0300

    Buffers - Video 4

--- learnopengl/src/main.cpp
+++ learnopengl/src/main.cpp
@@ -28,17 +28,26 @@ int main(void)
 
 	std::cout << "Version: " << glGetString(GL_VERSION) << std::endl;
 
+	/* set some buffers */
+	float positions[6] = {
+		-0.5f, -0.5f,
+		0.0f, 0.5f,
+		0.5f, -0.5f
+	};
+
+	unsigned int bufferId;
+	glGenBuffers(1, &bufferId);
+	glBindBuffer(GL_ARRAY_BUFFER, bufferId);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
