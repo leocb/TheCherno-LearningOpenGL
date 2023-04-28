4715786a682f8284676eb7b1b86cf506412b53cd
Leonardo Bottaro <leonardo.bottaro@oracle.com>
2023-04-28 20:20:01 -0300

    uniform 1f

--- learnopengl/src/Shader.cpp
+++ learnopengl/src/Shader.cpp
@@ -28,10 +28,16 @@ void Shader::Unbind() const
 	GLCall(glUseProgram(0));
 }
 
+void Shader::SetUniform1f(const std::string& name, float value)
+{
+	unsigned int location = GetUniformLocation(name);
+	GLCall(glUniform1f(location, value));
+}
+
 void Shader::SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3)
 {
 	unsigned int location = GetUniformLocation(name);
-	GLCall(glUniform4f(location, v0, v1, v2, v3)); // define a "default" value (necessary?)
+	GLCall(glUniform4f(location, v0, v1, v2, v3));
 }
 
 int Shader::GetUniformLocation(const std::string& name)
